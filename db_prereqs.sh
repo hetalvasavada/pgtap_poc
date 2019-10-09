c39a6d59bf63f675fcd05f338ea6069b74ae5d18
Naina Prabhu <nprabhu@planittesting.com>
2019-10-09 16:08:56 +1100

    Update db_prereqs.sh

--- db_prereqs.sh
+++ db_prereqs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-psql -U postgres -d testdb -c "CREATE user root"
-psql -U postgres -d testdb -c "create database root"
-psql -U postgres -d testdb -c "ALTER USER root SUPERUSER CREATEDB;"
-psql -U postgres -d testdb -c "CREATE TABLE account(   user_id serial PRIMARY KEY,   username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL,   email VARCHAR (355) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP);"
+psql -U postgres -c "CREATE user root"
+psql -U postgres -c "create database root"
+psql -U postgres -c "ALTER USER root SUPERUSER CREATEDB;"
+psql -U postgres -c "create extension if not exists pgtap;"
+psql -U postgres -c "CREATE TABLE account(   user_id serial PRIMARY KEY,   username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL,   email VARCHAR (355) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP);"
 echo "Done........."
