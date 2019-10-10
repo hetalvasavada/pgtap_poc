154f3fca7a697ab1513e4282c11bc119869d635d
Hetal Vasavada <hvasavada@planittesting.com>
2019-10-11 10:25:15 +1100

    Update function1.sql

--- src/sample_schema1/functions/function1.sql
+++ src/sample_schema1/functions/function1.sql
@@ -1,4 +1,16 @@
-create or replace function sample_schema1.accept_invite_to_list(userId integer, userVersion integer, inviteId integer)
-  returns boolean
-language plpgsql
-as $$
+CREATE OR REPLACE FUNCTION set_sensor_log(
+  p_location VARCHAR, p_reading BIGINT
+)
+RETURNS BOOLEAN AS
+$$
+BEGIN
+  IF p_reading > 500 THEN
+    RAISE EXCEPTION '"%" is an invalid reading!', p_reading;
+  END IF;
+
+  INSERT INTO sensor_log (location, reading, reading_date)
+    VALUES (p_location, p_reading, now());
+
+  RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
