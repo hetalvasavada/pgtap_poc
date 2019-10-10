cd8747acb12efbbb894937f0e87ec03ea42f3e2d
Hetal Vasavada <hvasavada@planittesting.com>
2019-10-10 11:59:08 +1100

    Update table1.sql

--- src/sample_schema1/tables/table1.sql
+++ src/sample_schema1/tables/table1.sql
@@ -3,17 +3,17 @@ RETURNS void AS
 $func$
 BEGIN
 IF EXISTS (SELECT * FROM pg_catalog.pg_tables 
-WHERE schemaname = 'sample_ctx'
+WHERE schemaname = 'sample_schema1'
 AND tablename = 'mv_motor_company') THEN
-RAISE NOTICE 'Table sample_ctx.mv_motor_company already exists.';
+RAISE NOTICE 'Table sample_schema1.mv_motor_company already exists.';
 ELSE
-CREATE TABLE sample_ctx.mv_motor_company (
+CREATE TABLE sample_schema1.mv_motor_company (
 userid_test integer PRIMARY KEY,
 company varchar NULL,
 subcoy int2 NULL,
 state bpchar(2) NULL,
 price numeric CHECK (price > 0),
-product_no integer REFERENCES sample_ctx.mv_motor_company_pk (userid_test)
+product_no integer REFERENCES sample_schema1.mv_motor_company_pk (userid_test)
 );
 END IF;
 END
