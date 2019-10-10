2f9949fdd9eff73986d4fc7426043f99ef5a42d4
Hetal Vasavada <hvasavada@planittesting.com>
2019-10-10 15:14:32 +1100

    Update db_prereqs.sh

--- db_prereqs.sh
+++ db_prereqs.sh
@@ -1,7 +1,7 @@
 POSTGRES_HOST=$1
 POSTGRES_HOST=$2
 
-psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "CREATE user root
+psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "CREATE user root"
 psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "create database root"
 psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "ALTER USER root SUPERUSER CREATEDB;"
 psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "create extension if not exists pgtap;"
